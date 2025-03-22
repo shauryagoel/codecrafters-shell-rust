6f26bc840bab8d27e0b7111173d6a12bf8465d66
Shaurya Goel <shoray.goel@gmail.com>
2025-03-22 13:08:10 +0530

    Add echo and refactor

--- src/main.rs
+++ src/main.rs
@@ -12,15 +12,16 @@ fn main() -> ExitCode {
         print!("$ ");
         stdout.flush().unwrap();
         stdin.read_line(&mut input).unwrap(); // Wait for user input
+        let trimmed_input = input.trim(); // Trim also removes the \n when pressing enter to run the command
 
-        // Trim removes the \n when pressing enter to run the command
-        match input.trim() {
-            "exit 0" => return ExitCode::from(0), // Return exit code from the program and exit it
-            _ => {
-                println!("{}: command not found", input.trim());
-            }
+        match trimmed_input.split_once(" ") {
+            Some(x) => match x.0 {
+                "exit" => return ExitCode::from(x.1.as_bytes()[0] - 48), // Return exit code from the program and exit it
+                "echo" => println!("{}", x.1),
+                _ => println!("{} {}: command not found", x.0, x.1),
+            },
+            None => println!("{}: command not found", trimmed_input),
         }
-
         input.clear(); // Clear the input string so that it can be used again without re-declaring the variable
     }
 }
