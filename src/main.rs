8b5c3e0ad3923a01180db9a20e0aeccf0d6c8552
Shaurya Goel <shoray.goel@gmail.com>
2025-03-23 12:17:00 +0530

    Refactor whole code

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
-#[allow(unused_imports)]
 use std::io::{self, Write};
 
 use std::env;
 use std::fs;
 use std::path::Path;
-use std::process::Command;
-use std::process::ExitCode;
+use std::process::{Command, ExitCode};
+
+// Parse error code from the Optional str (will be None if the exit command was not given any exit code)
+// Set default of 0 status code if received no status code
+// If unable to parse the string of status code to u8 then give 1 status code
+fn get_exit_code(str_code: Option<&&str>) -> u8 {
+    str_code.unwrap_or(&"0").parse::<u8>().unwrap_or(1)
+}
 
 // Return absolute path of the command if found in the PATH environment variable
 fn get_path(command_name: &str) -> Option<String> {
@@ -21,7 +26,8 @@ fn get_path(command_name: &str) -> Option<String> {
     None
 }
 
-fn echo(args: &str) {
+fn builtin_echo(args: &str) {
+    // Handle case of args which contains double single quotes and also no quotes
     let parsed_string: String = if args.starts_with('\'') && args.ends_with('\'') {
         args.split('\'').filter(|&x| !x.is_empty()).collect()
     } else {
@@ -32,28 +38,41 @@ fn echo(args: &str) {
     println!("{}", parsed_string);
 }
 
-// Function to change the directory (implemention of cd)
-fn change_directory(path: &str) {
+fn builtin_cd(path: Option<&&str>) {
     // TODO: Maybe try `Cow` to avoid using String
-    let cleaned_path = if path == "~" {
+    let corrected_path = if path.unwrap_or(&"~") == &"~" {
         env::var("HOME").unwrap()
     } else {
-        String::from(path)
+        path.unwrap().to_owned().to_owned()
     };
 
-    let path_obj = Path::new(cleaned_path.as_str());
+    let path_obj = Path::new(corrected_path.as_str());
 
-    // Changes the current directory, if error occurs like no directory exists, then, print an error
+    // Changes the current directory
+    // if error occurs like no directory exists, then, print an error
     if env::set_current_dir(path_obj).is_err() {
-        println!("cd: {}: No such file or directory", path);
+        println!("cd: {}: No such file or directory", corrected_path);
+    }
+}
+
+// Implement the type command
+fn builtin_type(command: &str) {
+    // Command implemented in the current program are called builtin commands
+    // NOTE: If adding new builtin command, make sure to add it below
+    let builtin_commands = ["exit", "echo", "type", "pwd", "cd"];
+
+    if builtin_commands.contains(&command) {
+        println!("{} is a shell builtin", command);
+    } else if let Some(command_path) = get_path(command) {
+        println!("{} is {}", command, command_path)
+    } else {
+        println!("{}: not found", command);
     }
 }
 
 fn main() -> ExitCode {
     let stdin = io::stdin();
     let mut stdout = io::stdout();
-
-    let builtin_commands = ["exit", "echo", "type", "pwd", "cd"];
 
     let mut input = String::new();
     loop {
@@ -63,11 +82,6 @@ fn main() -> ExitCode {
 
         // Sanitize the input
         let trimmed_input = input.trim(); // Trim also removes the \n when pressing enter to run the command
-                                          // let mut trimmed_input_it = trimmed_input.split_ascii_whitespace();
-                                          // let command = trimmed_input_it.next().unwrap();
-                                          // let mut args: String = trimmed_input_it.map(|x| String::from(x) + " ").collect();
-                                          // args.pop(); // Remove the extra space at the end
-
         let command_args = trimmed_input.split_once(" ");
 
         if command_args.is_none() && trimmed_input.is_empty() {
@@ -82,24 +96,17 @@ fn main() -> ExitCode {
         };
 
         // Handle double single quotes in args
-        let parsed_args: Vec<_> = args.split('\'').filter(|&x| !x.is_empty()).collect();
+        let parsed_args: Vec<&str> = args.split('\'').filter(|&x| !x.is_empty()).collect();
+        // println!("{:?}", parsed_args);
 
         match command {
-            "exit" => return ExitCode::from(args.as_bytes()[0] - 48), // Return exit code from the program and exit it TODO: use parse
-            "echo" => echo(args),
+            "exit" => return ExitCode::from(get_exit_code(parsed_args.first())),
+            "echo" => builtin_echo(args),
             "pwd" => println!("{}", env::current_dir().unwrap().display()),
-            "cd" => change_directory(args),
-            "type" => {
-                if builtin_commands.contains(&args) {
-                    println!("{} is a shell builtin", args);
-                } else if let Some(command_path) = get_path(args) {
-                    println!("{} is {}", args, command_path)
-                } else {
-                    println!("{}: not found", args);
-                }
-            }
+            "cd" => builtin_cd(parsed_args.first()),
+            "type" => builtin_type(args),
             _ => {
-                // Run arbitrary command if found in PATH
+                // Run the command in a subshell if found in PATH
                 if get_path(command).is_some() {
                     let _ = Command::new(command)
                         .args(parsed_args.iter().filter(|&&x| !x.trim().is_empty()))
@@ -107,12 +114,7 @@ fn main() -> ExitCode {
                         .expect("Command failed to run")
                         .wait();
                 } else {
-                    // This is done instead of using trimmed_input because it may not be sanitized
-                    let mut message = String::from(command);
-                    if !args.is_empty() {
-                        message += " {}";
-                    }
-                    println!("{}: command not found", message)
+                    println!("{}: command not found", trimmed_input)
                 }
             }
         }
